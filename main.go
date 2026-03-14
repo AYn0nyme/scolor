f5e4e76021a1832499c98c703df3dee211a57113
SnowsSky <snowsky473@gmail.com>
2026-03-14 17:18:06 +0100

    Added scolor.color.DisplayText()

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import scolor "github.com/SnowsSky/scolor/pkg"
 
 func main() {
-	scolor.DisplayText("red", "nob")
+	scolor.DisplayText("red", "pls fruit")
+	scolor.DisplayText("red", "ez no")
 }
--- pkg/color.go
+++ pkg/color.go
@@ -2,6 +2,10 @@ package pkg
 
 import "fmt"
 
+type color struct {
+	Color string
+}
+
 var Colors = map[string]string{
 	"red":     "\033[31m",
 	"green":   "\033[32m",
@@ -12,6 +16,14 @@ var Colors = map[string]string{
 	"gray":    "\033[37m",
 	"white":   "\033[97m",
 }
+var Red = color{"red"}
+var Green = color{"green"}
+var Yellow = color{"green"}
+var Blue = color{"blue"}
+var Magenta = color{"magenta"}
+var Cyan = color{"cyan"}
+var Gray = color{"gray"}
+var White = color{"white"}
 var Reset = "\033[0m"
 
 func DisplayText(color string, text string) {
@@ -19,6 +31,10 @@ func DisplayText(color string, text string) {
 		panic("Color not found")
 	}
 
-	fmt.Printf("%s%s%s", Colors[color], text, Reset)
+	fmt.Printf("%s%s%s ", Colors[color], text, Reset)
 
+}
+
+func (c color) DisplayText(text string) {
+	fmt.Printf("%s%s%s ", c, text, Reset)
 }
