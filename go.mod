aa0e2af22b1181a364e4b8635770c34384dadc9a
SnowsSky <snowsky473@gmail.com>
2026-03-14 16:39:02 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SnowsSky/scolor
+
+go 1.25.5
---
+++ pkg/color.go
@@ -0,0 +1,23 @@
+package pkg
+
+import "fmt"
+
+var Colors = map[string]string{
+	"red":     "\033[31m",
+	"green":   "\033[32m",
+	"yellow":  "\033[33m",
+	"blue":    "\033[34m",
+	"magenta": "\033[35m",
+	"cyan":    "\033[36m",
+	"gray":    "\033[37m",
+	"white":   "\033[97m",
+}
+
+func DisplayText(color string, text string) {
+	if _, ok := Colors[color]; !ok {
+		panic("Color not found")
+	}
+
+	fmt.Printf("%s%s", Colors[color], text)
+
+}
